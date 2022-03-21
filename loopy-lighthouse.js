4fb6f61c10f8afd11b6ab3bd036dcbf98a7ea07f
Jennifer Manwell <manwell.fortier@gmail.com>
2022-03-21 06:29:37 -0400

    updated for stretch activity loopy lighthouse 2

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,14 +1,17 @@
-for(let i = 100;i <= 200;i++)
-{
-  if(i % 3 === 0 && i % 4 !== 0 ){
-    console.log('Loopy');
-  }else if (i % 3 !== 0 && i % 4 === 0){
-    console.log('Lighthouse');
-  }else if(i % 3 === 0 && i % 4 === 0){
-    console.log('LoopyLighthouse');
-  }else {
-    console.log(i);
+const loopyLighthouse = function(range, multiples, words){
+  for(let i = range[0];i <= range[1];i++)
+  {
+    if(i % multiples[0] === 0 && i % multiples[1] !== 0 ){
+      console.log(words[0]);
+    }else if (i % multiples[0] !== 0 && i % multiples[1] === 0){
+      console.log(words[1]);
+    }else if(i % multiples[0] === 0 && i % multiples[1] === 0){
+      console.log(words[0] + words[1]);
+    }else {
+      console.log(i);
+    }
   }
 }
 
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 
