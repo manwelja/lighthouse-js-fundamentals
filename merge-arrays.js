01d5a21b4f00d495af13a42243a8893377508dff
Jennifer Manwell <manwell.fortier@gmail.com>
2022-03-21 06:41:02 -0400

    Merge array stretch activity

---
+++ merge-arrays.js
@@ -0,0 +1,10 @@
+const merge = function(arr1, arr2){
+  const modArray = arr1.slice();
+  modArray.push(...arr2);
+  modArray.sort();
+  return modArray;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
